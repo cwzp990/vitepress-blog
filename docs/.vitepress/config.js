a3c4b553cbd054f8bd70a6fa33bf037b55b2bb39
cwzp990 <cwzp990@163.com>
2023-03-10 18:10:37 +0800

    feat(setting): vitepress setting update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -11,6 +11,13 @@ export default {
 
   // 主题配置
   themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "源码系列", link: "/code/" },
+      { text: "笔记随想", link: "/note/" },
+      { text: "工作总结", link: "/daily/" },
+      { text: "关于我", link: "/me/" },
+    ],
     socialLinks: [
       //右上角图标和链接，icon 可用svg 配置
       {
@@ -18,6 +25,10 @@ export default {
         link: "https://github.com/cwzp990",
       },
     ],
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2022-10-20～present cwzp990",
+    },
     lastUpdatedText: "更新时间",
     siteTitle: "桔子的前端小屋",
     nav: [
@@ -25,7 +36,26 @@ export default {
       { text: "首页", link: "/" },
     ],
     // 侧边导航
-    sidebar: [],
+    sidebar: {
+      "/code/": [
+        {
+          text: "源码阅读",
+          items: [],
+        },
+      ],
+      "/note/": [
+        {
+          text: "笔记记录",
+          items: [],
+        },
+      ],
+      "/daily/": [
+        {
+          text: "日常工作总结",
+          items: [],
+        },
+      ],
+    },
     outlineTitle: "目录",
   },
 };
--- docs/index.md
+++ docs/index.md
@@ -4,14 +4,17 @@ title: 桔子的前端小屋
 description: fe/blog/vue/react
 
 hero:
-  name: 桔子的前端小屋🎉
-  text: 桔子的成长之路
+  name: 桔子🎉
+  text: 记录生活，记录学习
   tagline: 越努力越幸运
   actions:
     - theme: brand
-      text: 开始
-      link: /page/index
+      text: 更多详情
+      link: /note/
     - theme: alt
       text: 访问github
       link: https://github.com/cwzp990
+    - theme: alt
+      text: 关于我
+      link: /me/
 ---
